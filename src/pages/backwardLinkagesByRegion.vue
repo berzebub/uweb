4a6e54328a15016491ebd3dd006ca40e1c2b3ea2
art <pakkawat.kh@gmail.com>
2020-08-15 13:39:56 +0700

    update

--- src/pages/backwardLinkagesByRegion.vue
+++ src/pages/backwardLinkagesByRegion.vue
@@ -4,7 +4,8 @@
     <header-menu :activeMenu="3"></header-menu>
     <importing-select></importing-select>
 
-    <div class="q-px-md">
+    <!-- Title box -->
+    <div class="q-px-md" style="margin:auto; max-width:1050px;width:95%;">
       <div class="q-pa-md" style="border-radius:5px;border:2px solid">
         <p class="font-graph" align="center">Where does imported content come from?</p>
         <p
@@ -23,12 +24,13 @@
         </p>
       </div>
 
+      <!-- Key policy questions -->
       <p align="center" class="font-graph q-py-lg">Key policy questions</p>
-      <p class="font-content q-px-sm">
+      <p class="font-content q-px-sm cursor-pointer" v-scroll-to="'#importedcountry'">
         1.
         <u>Where does Thailand’s imported content come from in exports to a selected importer ?</u>
       </p>
-      <p class="font-content q-px-sm">
+      <p class="font-content q-px-sm cursor-pointer" v-scroll-to="'#importedregion'">
         2.
         <u>Where do South-East Asian economies’ imported content come from in exports to a selected importer ?</u>
       </p>
@@ -38,49 +40,18 @@
     <hr />
     <div style="height:30px"></div>
 
-    <p
-      class="font-graph"
-      align="center"
-    >Where does Thailand’s imported content in exports to China come from?</p>
-    <p
-      class="font-content q-px-lg"
-    >Gross exports of Thailand in All sector(s) to China amount to *$40* billion in *year*. Of these exports, *$8* billion is imported content that comes from other economies, mainly United States of America (*19.05*%), Hong Kong (*10.9*%), Japan (*5.61*%), Rep. of Korea (*3.98*%) and Germany (*4.39*%).</p>
-
-    <div class="row items-center">
-      <div class="col">
-        <div id="container"></div>
-        <div align="left" class="font-footer q-px-md">Gross exports to China : $40 B</div>
-      </div>
-      <div style="width:300px" class="q-px-sm">
-        <div
-          class="row items-center q-pt-md"
-          v-for="(items,index) in colorListWithLabel"
-          :key="index"
-        >
-          <div style="width:45px;height:30px" :class="items.color"></div>
-          <div class="col q-pl-sm">{{ items.label }}</div>
-        </div>
-      </div>
+    <!-- Where does Thailand's imported content -->
+    <div style="width:90%;margin:auto;max-width:1200px" id="importedcountry">
+      <div id="container"></div>
     </div>
 
     <div style="height:30px"></div>
     <hr />
-    <div style="height:30px"></div>
+    <!-- Where does South-East Asian imported content -->
+    <div style="height:30px" id="importedregion"></div>
 
-    <div class="row items-center">
-      <div class="col">
-        <div id="container1"></div>
-      </div>
-      <div style="width:300px" class="q-px-sm">
-        <div
-          class="row items-center q-pt-md"
-          v-for="(items,index) in colorListWithLabel"
-          :key="index"
-        >
-          <div style="width:45px;height:30px" :class="items.color"></div>
-          <div class="col q-pl-sm">{{ items.label }}</div>
-        </div>
-      </div>
+    <div style="width:90%;margin:auto;max-width:1200px">
+      <div id="container1"></div>
     </div>
   </q-page>
 </template>
@@ -125,96 +96,413 @@ export default {
   },
   methods: {
     async setData() {
-      let getData = await Axios.get(
-        "https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-mortality.json"
-      );
-
-      let data = getData.data;
-      var points = [],
-        regionP,
-        regionVal,
-        regionI = 0,
-        countryP,
-        countryI,
-        causeP,
-        causeI,
-        region,
-        country,
-        cause,
-        causeName = {
-          "Communicable & other Group I": "Communicable diseases",
-          "Noncommunicable diseases": "Non-communicable diseases",
-          Injuries: "Injuries",
-        };
-
-      for (region in data) {
-        if (data.hasOwnProperty(region)) {
-          regionVal = 0;
-          regionP = {
-            id: "id_" + regionI,
-            name: region,
-            color: Highcharts.getOptions().colors[regionI],
-          };
-          countryI = 0;
-          for (country in data[region]) {
-            if (data[region].hasOwnProperty(country)) {
-              countryP = {
-                id: regionP.id + "_" + countryI,
-                name: country,
-                parent: regionP.id,
-              };
-              points.push(countryP);
-              causeI = 0;
-              for (cause in data[region][country]) {
-                if (data[region][country].hasOwnProperty(cause)) {
-                  causeP = {
-                    id: countryP.id + "_" + causeI,
-                    name: causeName[cause],
-                    parent: countryP.id,
-                    value: Math.round(+data[region][country][cause]),
-                  };
-                  regionVal += causeP.value;
-                  points.push(causeP);
-                  causeI = causeI + 1;
-                }
-              }
-              countryI = countryI + 1;
-            }
-          }
-          regionP.value = Math.round(regionVal / countryI);
-          points.push(regionP);
-          regionI = regionI + 1;
-        }
-      }
       Highcharts.chart("container", {
+        chart: {
+          height: (9 / 16) * 100 + "%", // 16:9 ratio
+          style: { fontFamily: "roboto" },
+        },
         series: [
           {
             type: "treemap",
             layoutAlgorithm: "squarified",
-            allowDrillToNode: true,
-            animationLimit: 1000,
-            dataLabels: {
-              enabled: false,
-            },
-            levelIsConstant: false,
+            alternateStartingDirection: true,
             levels: [
               {
                 level: 1,
+                // layoutAlgorithm: "sliceAndDice",
                 dataLabels: {
                   enabled: true,
+                  align: "left",
+                  verticalAlign: "top",
+                  style: {
+                    fontSize: "15px",
+                    fontWeight: "bold",
+                  },
                 },
-                borderWidth: 3,
               },
             ],
-            data: points,
+
+            data: [
+              {
+                id: "A",
+                name: "Aisa-Pacific",
+                color: "#2381B8",
+              },
+              {
+                id: "B",
+                name: "Europe",
+                color: "#EB1E63",
+              },
+              {
+                id: "C",
+                name: "North America",
+                color: "#F99704",
+              },
+              {
+                id: "D",
+                name: "Latin America",
+                color: "#2D9687",
+              },
+              {
+                id: "E",
+                name: "Rest of the world",
+                color: "#9C26B3",
+              },
+              {
+                name: "Bhutan",
+                parent: "A",
+                value: 1.3,
+              },
+              {
+                name: "Hongkong",
+                parent: "A",
+                value: 10.9,
+              },
+              {
+                name: "Japan",
+                parent: "A",
+                value: 5.61,
+              },
+              {
+                name: "Bangladesh",
+                parent: "A",
+                value: 3.21,
+              },
+              {
+                name: "Brunei Darussalam",
+                parent: "A",
+                value: 1.21,
+              },
+              {
+                name: "Cambodia",
+                parent: "A",
+                value: 0.21,
+              },
+              {
+                name: "Cyprus",
+                parent: "A",
+                value: 0.31,
+              },
+              {
+                name: "India",
+                parent: "A",
+                value: 3.31,
+              },
+              {
+                name: "Indonesia",
+                parent: "A",
+                value: 2.31,
+              },
+              {
+                name: "Kazakhstan",
+                parent: "A",
+                value: 1.51,
+              },
+              {
+                name: "Kyrgyz Republic",
+                parent: "A",
+                value: 0.51,
+              },
+              {
+                name: "Republic of Korea",
+                parent: "A",
+                value: 2.51,
+              },
+              {
+                name: "Malaysia",
+                parent: "A",
+                value: 1.51,
+              },
+              {
+                name: "Maldives",
+                parent: "A",
+                value: 1.01,
+              },
+              {
+                name: "Mongolia",
+                parent: "A",
+                value: 0.81,
+              },
+              {
+                name: "Nepal",
+                parent: "A",
+                value: 0.81,
+              },
+              {
+                name: "Pakistan",
+                parent: "A",
+                value: 0.81,
+              },
+              {
+                name: "Philippines",
+                parent: "A",
+                value: 2.81,
+              },
+              {
+                name: "People's Republic of China",
+                parent: "A",
+                value: 8.81,
+              },
+              {
+                name: "Singapore",
+                parent: "A",
+                value: 2.81,
+              },
+              {
+                name: "Sri Lanka",
+                parent: "A",
+                value: 0.81,
+              },
+              {
+                name: "Austria",
+                parent: "B",
+                value: 1.8,
+              },
+              {
+                name: "Belgium",
+                parent: "B",
+                value: 2.5,
+              },
+              {
+                name: "Bulgaria",
+                parent: "B",
+                value: 1,
+              },
+              {
+                name: "Czech Republic",
+                parent: "B",
+                value: 1.4,
+              },
+              {
+                name: "Denmark",
+                parent: "B",
+                value: 1.6,
+              },
+              {
+                name: "Estonia",
+                parent: "B",
+                value: 0.3,
+              },
+              {
+                name: "Finland",
+                parent: "B",
+                value: 1.3,
+              },
+              {
+                name: "France",
+                parent: "B",
+                value: 1.4,
+              },
+              {
+                name: "Germany",
+                parent: "B",
+                value: 4.3,
+              },
+              {
+                name: "Greece",
+                parent: "B",
+                value: 0.3,
+              },
+              {
+                name: "Croatia",
+                parent: "B",
+                value: 0,
+              },
+              {
+                name: "Hungary",
+                parent: "B",
+                value: 1.1,
+              },
+              {
+                name: "Ireland",
+                parent: "B",
+                value: 0,
+              },
+              {
+                name: "Italy",
+                parent: "B",
+                value: 2.1,
+              },
+              {
+                name: "Lithuania",
+                parent: "B",
+                value: 0,
+              },
+              {
+                name: "Luxembourg",
+                parent: "B",
+                value: 0,
+              },
+              {
+                name: "Latvia",
+                parent: "B",
+                value: 0,
+              },
+              {
+                name: "Malta",
+                parent: "B",
+                value: 0.1,
+              },
+              {
+                name: "Netherlands",
+                parent: "B",
+                value: 2.1,
+              },
+              {
+                name: "Norway",
+                parent: "B",
+                value: 1.1,
+              },
+              {
+                name: "Poland",
+                parent: "B",
+                value: 0,
+              },
+              {
+                name: "Portugal",
+                parent: "B",
+                value: 2,
+              },
+              {
+                name: "Romania",
+                parent: "B",
+                value: 2.3,
+              },
+              {
+                name: "Russia",
+                parent: "B",
+                value: 1.4,
+              },
+              {
+                name: "Spain",
+                parent: "B",
+                value: 3.2,
+              },
+              {
+                name: "Slovak Republic",
+                parent: "B",
+                value: 0,
+              },
+              {
+                name: "Slovenia",
+                parent: "B",
+                value: 0.1,
+              },
+              {
+                name: "Sweden",
+                parent: "B",
+                value: 1.1,
+              },
+              {
+                name: "Switzerland",
+                parent: "B",
+                value: 0.3,
+              },
+              {
+                name: "United States",
+                parent: "C",
+                value: 3.8,
+              },
+              {
+                name: "Canada",
+                parent: "C",
+                value: 2.7,
+              },
+              {
+                name: "Argentina",
+                parent: "D",
+                value: 1.8,
+              },
+              {
+                name: "Bolivia",
+                parent: "D",
+                value: 0.9,
+              },
+              {
+                name: "Brazil",
+                parent: "D",
+                value: 2.9,
+              },
+              {
+                name: "Bolivia",
+                parent: "D",
+                value: 0.9,
+              },
+              {
+                name: "Chile",
+                parent: "D",
+                value: 1.9,
+              },
+              {
+                name: "Colombia",
+                parent: "D",
+                value: 0.3,
+              },
+              {
+                name: "Ecuador",
+                parent: "D",
+                value: 0,
+              },
+              {
+                name: "Maxico",
+                parent: "D",
+                value: 0.8,
+              },
+              {
+                name: "Paraguay",
+                parent: "D",
+                value: 0.3,
+              },
+              {
+                name: "Peru",
+                parent: "D",
+                value: 0.7,
+              },
+              {
+                name: "Uruguay",
+                parent: "D",
+                value: 0.9,
+              },
+              {
+                name: "Venezuela",
+                parent: "D",
+                value: 0,
+              },
+              {
+                name: "Rest of the Latin America",
+                parent: "D",
+                value: 0.1,
+              },
+              {
+                name: "Australia",
+                parent: "E",
+                value: 2.4,
+              },
+              {
+                name: "Rest of the world",
+                parent: "E",
+                value: 0.4,
+              },
+            ],
           },
         ],
-        subtitle: {
-          text: "Imported content in exports to China : $8B",
-          align: "left",
-        },
         title: {
-          text: "",
+          style: {
+            fontSize: "24px",
+          },
+          text:
+            "Where does Thailand's imported content in exports to China come from?",
+        },
+        credits: {
+          enabled: false,
+        },
+        subtitle: {
+          style: {
+            fontSize: "14px",
+          },
+          text:
+            "Gross exports of Thailand in All sector(s) to China amount to *$40* billion in *year*. Of these exports, *$8* billion is imported content that comes from other economies, mainly United States of America (*19.05*%), Hong Kong (*10.9*%), Japan (*5.61*%), Rep. of Korea (*3.98*%) and Germany (*4.39*%). <br>imported content in exports to China: $8B / Gross exports to China: $40B",
+          align: "center",
         },
       });
     },
@@ -223,10 +511,9 @@ export default {
         chart: {
           type: "column",
           height: (9 / 16) * 100 + "%", // 16:9 ratio
+          style: { fontFamily: "roboto" },
         },
-        title: {
-          text: "What happens to South-East Asian economies’ exports to China?",
-        },
+
         xAxis: {
           labels: {
             rotation: -90,
@@ -242,6 +529,9 @@ export default {
             "Thailand",
             "Vietnam",
           ],
+        },
+        credits: {
+          enabled: false,
         },
         yAxis: {
           min: 0,
@@ -260,19 +550,9 @@ export default {
             },
           },
         },
-        legend: {
-          // align: "right",
-          // verticalAlign: "right",
-          // floating: true,
-          // backgroundColor:
-          //   Highcharts.defaultOptions.legend.backgroundColor || "white",
-          // borderColor: "#CCC",
-          // borderWidth: 1,
-          // shadow: false,
-          enabled: false,
-        },
+
         tooltip: {
-          headerFormat: "<b>{point.x}</b><br/>",
+          headerFormat: "<b>{point.x}</pimported><br/>",
           pointFormat: "{series.name}: {point.y}<br/>Total: {point.stackTotal}",
         },
         plotOptions: {
@@ -283,6 +563,7 @@ export default {
             },
           },
         },
+
         series: [
           {
             name: "Rest of the world",
@@ -312,6 +593,9 @@ export default {
           },
         ],
         title: {
+          style: {
+            fontSize: "24px",
+          },
           text:
             "Where do South-East Asian economies’ imported content in exports to China come from  ?",
         },
