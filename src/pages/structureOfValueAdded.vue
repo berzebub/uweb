3b922e6e137f918e66a70aac1dd4bb0eb07a4f68
Tanin <ber_zebub@hotmail.com>
2020-09-22 23:28:09 +0700

    Re-HTML in structure / participation

--- src/pages/structureOfValueAdded.vue
+++ src/pages/structureOfValueAdded.vue
@@ -1,10 +1,192 @@
 <template>
+  <q-page>
+    <global-value-chains-header></global-value-chains-header>
 
-<q-page>
-     <global-value-chains-header></global-value-chains-header>
-    
-     <global-value-chains-menu :activeMenu=1></global-value-chains-menu>
-</q-page>
+    <global-value-chains-menu :activeMenu="1"></global-value-chains-menu>
+
+    <!-- DROPDOWN SELECTION -->
+    <div class="bg12" style="height:180px">
+      <div class="row q-pt-md justify-center">
+        <div class="col-3 q-px-md">
+          <div>Exporting economy</div>
+          <div>
+            <q-select
+              bg-color="white"
+              v-model="exportingSelected"
+              dense
+              :options="exportingOptions"
+              outlined
+            >
+              <template v-slot:selected-item="props">
+                <q-img :src="props.opt.flag" width="30px" />
+                <span class="q-mx-sm">{{props.opt.label}}</span>
+              </template>
+            </q-select>
+          </div>
+        </div>
+        <div class="col-3 q-px-md">
+          <div>Year</div>
+          <div>
+            <q-select
+              outlined
+              bg-color="white"
+              v-model="yearSelected"
+              dense
+              :options="exportingOptions"
+            ></q-select>
+          </div>
+        </div>
+      </div>
+
+      <div class="row q-pt-md justify-center">
+        <div class="col-3 q-px-md">
+          <div>Importing economy</div>
+          <div>
+            <q-select
+              bg-color="white"
+              v-model="importingSelected"
+              dense
+              :options="exportingOptions"
+              outlined
+            >
+              <template v-slot:selected-item="props">
+                <q-img :src="props.opt.flag" width="30px" />
+                <span class="q-mx-sm">{{props.opt.label}}</span>
+              </template>
+            </q-select>
+          </div>
+        </div>
+        <div class="col-3 q-px-md">
+          <div>Exporting sector</div>
+          <div>
+            <q-select
+              bg-color="white"
+              v-model="sectorSelected"
+              dense
+              :options="exportingOptions"
+              outlined
+            ></q-select>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <div class="row q-px-md q-py-lg font-content">
+      <div class="col q-px-md" align="center">
+        <div
+          style="border:1px solid;height:60px"
+          class="flex flex-center cursor-pointer relative-position"
+          v-ripple
+        >Key policy questions</div>
+      </div>
+      <div class="col q-px-md" align="center">
+        <div
+          style="border:1px solid;height:60px"
+          class="flex flex-center cursor-pointer relative-position"
+          v-ripple
+        >Structure of value-added</div>
+      </div>
+      <div class="col q-px-md" align="center">
+        <div
+          style="border:1px solid;height:60px"
+          class="flex flex-center cursor-pointer relative-position"
+          v-ripple
+        >Measuring trade balance</div>
+      </div>
+      <div class="col q-px-md" align="center">
+        <div
+          style="border:1px solid;height:60px"
+          class="flex flex-center cursor-pointer relative-position"
+          v-ripple
+        >Comparison with sub-regional partners</div>
+      </div>
+    </div>
+    <!--  -->
+    <div class="row" style="height:340px">
+      <div class="col-3">
+        <q-img src="../../public/images/image-58.png"></q-img>
+      </div>
+      <div class="col flex flex-center" align="center" style="background-color:#E5E1E1">
+        <div class="divRight">
+          <p class="font-24" align="center">How do you disaggregate value-added in gross exports?</p>
+
+          <div
+            class="font-content q-pb-sm"
+            align="center"
+          >Argentina's gross exports can be divided into five major parts:</div>
+          <div class="row q-pt-md" style="width:95%; margin:auto;">
+            <div style="width:20%" align="center">Used in importer's comsumption</div>
+            <div style="width:20%" align="center">Used in domestic comsumption</div>
+            <div style="width:20%" align="center">Used in importer's export production</div>
+            <div style="width:20%" align="center">Imported content</div>
+            <div
+              style="width:20%"
+              align="center"
+            >Double counted exports from repeated border crossing</div>
+          </div>
+          <div class="bracketLeft"></div>
+          <div class="bracketRight"></div>
+          <div class="textLeft font-content">Argentina's valued-added in exports</div>
+          <div class="textRight font-content">GVC releated exports</div>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <div style="padding-top:50px;padding-bottom:50px">
+      <p align="center" class="font-24">Key policy questions</p>
+
+      <div class="font-content" style="padding :0px 60px">
+        <div v-scroll-to="'#structure'" class="cursor-pointer">
+          1.
+          <u>What happens to {{displayExportingEconomy}}’s exports to a selected importer?</u>
+        </div>
+        <div class="cursor-pointer q-py-sm" v-scroll-to="'#comparison'">
+          2.
+          <u>What happens to {{continent}} economics’ exports to a selected imported?</u>
+        </div>
+        <div class="cursor-pointer" v-scroll-to="'#measuring'">
+          3.
+          <u>How does {{displayExportingEconomy}}’s domestic value-added and gross trade balance with the selected imported differ?</u>
+        </div>
+      </div>
+    </div>
+    <!-- GRAPH1 -->
+    <div style="height:30px" id="structure"></div>
+    <div style="width:90%;margin:auto;max-width:1200px">
+      <div align="center" class="q-pa-lg" v-if="!isStructureChart">
+        <q-spinner-pie color="primary" size="100px" />
+      </div>
+      <div v-show="isStructureChart">
+        <div id="container"></div>
+      </div>
+    </div>
+    <hr />
+    <!-- GRAPH2 -->
+
+    <div id="comparison" style="height:30px"></div>
+    <div style="width:90%;margin:auto;max-width:1200px">
+      <div align="center" class="q-pa-lg" v-if="!isComparisonChart">
+        <q-spinner-pie color="primary" size="100px" />
+      </div>
+      <div v-show="isComparisonChart">
+        <div id="container1"></div>
+      </div>
+    </div>
+    <hr />
+
+    <!--GRAPH3  -->
+    <div id="measuring" style="height:30px"></div>
+    <div style="width:90%;margin:auto;max-width:1200px">
+      <div align="center" class="q-pa-lg" v-if="!isMeasuringChart">
+        <q-spinner-pie color="primary" size="100px" />
+      </div>
+      <div v-show="isMeasuringChart">
+        <div id="container2"></div>
+      </div>
+    </div>
+    <!-- FOOTER -->
+    <my-footer></my-footer>
+  </q-page>
   <!-- <q-page
     class="container"
     :class="!isShowPage || isShowErrorWarning ? 'bg-loading' : 'bg-white'"
@@ -169,7 +351,7 @@ importing economy"
         <div style="height:30px"></div>
       </div>
     </div>
-  </q-page> -->
+  </q-page>-->
 </template>
 
 <script>
@@ -179,8 +361,9 @@ import importingSelect from "../components/importEconomySelect";
 import Axios from "axios";
 import errorPage from "../components/error-page";
 // new
-import globalValueChainsHeader from "../components/globalValueChainsHeader"
-import globalValueChainsMenu from "../components/menu"
+import globalValueChainsHeader from "../components/globalValueChainsHeader";
+import globalValueChainsMenu from "../components/menu";
+import myFooter from "../components/footer";
 export default {
   components: {
     appBar,
@@ -189,9 +372,32 @@ export default {
     errorPage,
     globalValueChainsHeader,
     globalValueChainsMenu,
+    myFooter,
   },
   data() {
     return {
+      // NEW
+      exportingOptions: [
+        {
+          label: "Argentina",
+          value: "ARG",
+          flag:
+            "https://www.iconfinder.com/data/icons/ensign-11/512/16_Ensign_Flag_Nation_Argentina-512.png",
+        },
+
+        {
+          label: "United State",
+          value: "USA",
+          flag:
+            "https://www.iconfinder.com/data/icons/ensign-11/512/274_Ensign_Flag_Nation_states-512.png",
+        },
+      ],
+      exportingSelected: "",
+      yearSelected: "",
+      importingSelected: "",
+      sectorSelected: "",
+
+      // OLD
       isShowPage: false,
       countryOptions: [],
       importingEconomy: "",
@@ -235,29 +441,29 @@ export default {
     };
   },
   methods: {
-    checkShowPage() {
-      if (
-        this.displayExportingEconomy != "" &&
-        this.displayYear != "" &&
-        this.displayImportingEconomy != "" &&
-        this.displaySector != ""
-      ) {
-        this.isShowPage = true;
-        return true;
-      } else {
-        return false;
-      }
-    },
+    // checkShowPage() {
+    //   if (
+    //     this.displayExportingEconomy != "" &&
+    //     this.displayYear != "" &&
+    //     this.displayImportingEconomy != "" &&
+    //     this.displaySector != ""
+    //   ) {
+    //     this.isShowPage = true;
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    // },
     // Get Emit Data
-    getEmitData(val) {
-      // Exporting Economy
-      this.displayExportingEconomy = val.name;
-      this.exp_country = val.iso;
-      this.continent = val.region;
-      this.displayYear = val.year;
-      this.checkShowPage();
-      this.getStructureOfValue();
-    },
+    // getEmitData(val) {
+    //   // Exporting Economy
+    //   this.displayExportingEconomy = val.name;
+    //   this.exp_country = val.iso;
+    //   this.continent = val.region;
+    //   this.displayYear = val.year;
+    //   this.checkShowPage();
+    //   this.getStructureOfValue();
+    // },
 
     // Get Structure Of Value
     async getStructureOfValue() {
@@ -702,13 +908,9 @@ export default {
 .divRight {
   margin-left: 20px;
   width: 750px;
-  /* height: 300px; */
-  border: 2px solid black;
-  border-radius: 10px;
-  padding: 10px 0px;
 }
 .bracketLeft {
-  margin-left: 30px;
+  margin-left: -270px;
   width: 400px;
   height: 10px;
   border-left: 1px solid black;
@@ -725,12 +927,13 @@ export default {
   border-bottom: 1px solid black;
 }
 .textLeft {
-  margin-left: 80px;
+  margin-top: 20px;
+  margin-left: -470px;
   width: 200px;
   text-align: center;
 }
 .textRight {
-  margin-top: -30px;
+  margin-top: -50px;
   margin-left: 400px;
   width: 200px;
   text-align: center;
