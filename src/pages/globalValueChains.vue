c9e996c47958f369d8905a37ae03b7b79f9a8fef
MMasterZ <soulhunz@hotmail.com>
2020-09-27 13:54:09 +0700

    update links backward and forward

--- src/pages/globalValueChains.vue
+++ src/pages/globalValueChains.vue
@@ -133,15 +133,15 @@ export default {
           text: "What about backward linkages?",
           hover:
             "Find out where imported content used in exports comes from for your economy of choice. Examine this by region and sector. Compare across sub-regional partners ",
-          router: "/forward-linkages",
+          router: "/backward-linkages"
         },
         {
           icon: require("../../public/images/icon05.png"),
           text: "What about forward linkages?",
           hover:
             "Find out where your economy of choice contributes towards export production. Examine this by region and sector. Compare across sub-regional partners ",
-          router: "/backward-linkages",
-        },
+          router: "/forward-linkages"
+        }
       ],
       cardList2: [
         {
